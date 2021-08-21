340dbb3c9103fc7ceb4880382939116067dffe13
max6746 <kumar.mayank6746@gmail.com>
2021-08-22 01:10:01 +0530

    adding ar backend files

---
+++ augmented_reality_backend/ar.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+imgTar = cv2.imread('trgimg.jpg')
+# imgTarGry = cv2.cvtColor(imgTar, cv2.COLOR_BGR2GRAY)
+# cv2.imshow('img_gray', imgTarGry)
+# cv2.waitKey(0)
+
+
+myVid = cv2.VideoCapture('video.mp4')
+
+success, imgVideo = myVid.read()
+hT,wT,cT = imgTar.shape
+imgVideo = cv2.resize(imgVideo,(wT,hT))
+
+orb = cv2.ORB_create(nfeatures=10000)
+# kp1, des1 = orb.detectAndCompute(imgTarGry, None)
+# imgTarGry = cv2.drawKeypoints(imgTarGry,kp1,None)
+
+kp1, des1 = orb.detectAndCompute(imgTar, None)
+imgTar = cv2.drawKeypoints(imgTar,kp1,None)
+
+while True:
+    success, imgWebcam = cap.read()
+    imgAug = imgWebcam.copy()
+    # imgWebcamGry = cv2.cvtColor(imgWebcam, cv2.COLOR_BGR2GRAY)
+    # kp2, des2 = orb.detectAndCompute(imgWebcamGry, None)
+    # imgWebcamGry = cv2.drawKeypoints(imgWebcamGry,kp2,None)
+    kp2, des2 = orb.detectAndCompute(imgWebcam, None)
+    imgWebcam = cv2.drawKeypoints(imgWebcam,kp2,None)
+
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(des1, des2, k=2)
+    good =[]
+    for m,n in matches:
+        if m.distance < 0.75*n.distance:
+            good.append(m)
+    print(len(good))
+    # imgFeatures = cv2.drawMatches(imgTarGry, kp1, imgWebcamGry, kp2, good, None, flags=2)
+    imgFeatures = cv2.drawMatches(imgTar, kp1, imgWebcam, kp2, good, None, flags=2)
+
+    if len(good) > 5:
+        srcPts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1,1,2)
+        dstPts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1,1,2)
+
+        # matrix, mask = cv2.findHomography(srcPts, dstPts, cv2.RHO, 10)
+        matrix, mask = cv2.findHomography(srcPts, dstPts, cv2.LMEDS)
+        print(matrix)
+
+        pts = np.float32([[0,0], [0,hT], [wT,hT], [wT,0]]).reshape(-1,1,2)
+        dst = cv2.perspectiveTransform(pts, matrix)
+        img2 = cv2.polylines(imgWebcam, [np.int32(dst)], True, (255,0,255), 3)
+
+        imgWarp = cv2.warpPerspective(imgVideo, matrix, (imgWebcam.shape[1], imgWebcam.shape[0]))
+
+        maskNew = np.zeros((imgWebcam.shape[0], imgWebcam.shape[1]), np.uint8)
+        cv2.fillPoly(maskNew, [np.int32(dst)], (255,255,255))
+        maskInv = cv2.bitwise_not(maskNew)
+        imgAug = cv2.bitwise_and(imgAug, imgAug, mask=maskInv)
+        imgAug = cv2.bitwise_or(imgWarp, imgAug)
+
+    cv2.imshow('imgAug', imgAug)
+    cv2.imshow('imgWarp', imgWarp)
+    cv2.imshow('img2', img2)
+    cv2.imshow('imgFeatures', imgFeatures)
+
+    cv2.imshow('ImgTarget', imgTar)
+    cv2.imshow('myVid', imgVideo)
+    cv2.imshow('webcam', imgWebcam)
+    cv2.waitKey(0)
