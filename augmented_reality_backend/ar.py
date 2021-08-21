735ad1ef9de07fb9e8b8d054e7fb49f89d28590f
max6746 <kumar.mayank6746@gmail.com>
2021-08-22 01:50:37 +0530

    adding stackImages and switching to live stream

--- augmented_reality_backend/ar.py
+++ augmented_reality_backend/ar.py
@@ -10,16 +10,56 @@ imgTar = cv2.imread('trgimg.jpg')
 
 myVid = cv2.VideoCapture('video.mp4')
 
+
 success, imgVideo = myVid.read()
 hT,wT,cT = imgTar.shape
 imgVideo = cv2.resize(imgVideo,(wT,hT))
 
-orb = cv2.ORB_create(nfeatures=10000)
+orb = cv2.ORB_create(nfeatures=1000)
 # kp1, des1 = orb.detectAndCompute(imgTarGry, None)
 # imgTarGry = cv2.drawKeypoints(imgTarGry,kp1,None)
 
 kp1, des1 = orb.detectAndCompute(imgTar, None)
 imgTar = cv2.drawKeypoints(imgTar,kp1,None)
+
+
+def stackImages(imgArray,scale,lables=[]):
+    sizeW= imgArray[0][0].shape[1]
+    sizeH = imgArray[0][0].shape[0]
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                imgArray[x][y] = cv2.resize(imgArray[x][y], (sizeW,sizeH), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((sizeH, sizeW, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+            hor_con[x] = np.concatenate(imgArray[x])
+        ver = np.vstack(hor)
+        ver_con = np.concatenate(hor)
+    else:
+        for x in range(0, rows):
+            imgArray[x] = cv2.resize(imgArray[x], (sizeW, sizeH), None, scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        hor_con= np.concatenate(imgArray)
+        ver = hor
+    if len(lables) != 0:
+        eachImgWidth= int(ver.shape[1] / cols)
+        eachImgHeight = int(ver.shape[0] / rows)
+        print(eachImgHeight)
+        for d in range(0, rows):
+            for c in range (0,cols):
+                cv2.rectangle(ver,(c*eachImgWidth,eachImgHeight*d),(c*eachImgWidth+len(lables[d])*13+27,30+eachImgHeight*d),(255,255,255),cv2.FILLED)
+                cv2.putText(ver,lables[d],(eachImgWidth*c+10,eachImgHeight*d+20),cv2.FONT_HERSHEY_COMPLEX,0.7,(255,0,255),2)
+    return ver
+
+
 
 while True:
     success, imgWebcam = cap.read()
@@ -44,7 +84,7 @@ while True:
         srcPts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1,1,2)
         dstPts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1,1,2)
 
-        # matrix, mask = cv2.findHomography(srcPts, dstPts, cv2.RHO, 10)
+        # matrix, mask = cv2.findHomography(srcPts, dstPts, cv2.RANSAC, 2)
         matrix, mask = cv2.findHomography(srcPts, dstPts, cv2.LMEDS)
         print(matrix)
 
@@ -60,6 +100,8 @@ while True:
         imgAug = cv2.bitwise_and(imgAug, imgAug, mask=maskInv)
         imgAug = cv2.bitwise_or(imgWarp, imgAug)
 
+        imgStacked = stackImages(([imgWebcam, imgWarp], [imgWebcam, imgWarp]), 0.5)
+
     cv2.imshow('imgAug', imgAug)
     cv2.imshow('imgWarp', imgWarp)
     cv2.imshow('img2', img2)
@@ -68,4 +110,4 @@ while True:
     cv2.imshow('ImgTarget', imgTar)
     cv2.imshow('myVid', imgVideo)
     cv2.imshow('webcam', imgWebcam)
-    cv2.waitKey(0)
+    cv2.waitKey(1)
