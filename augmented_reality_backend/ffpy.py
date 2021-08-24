34e4236462df2ed9e14e1f8a1787053858dd094d
max6746 <kumar.mayank6746@gmail.com>
2021-08-24 22:58:37 +0530

    test ffpyplayer

--- augmented_reality_backend/ffpy.py
+++ augmented_reality_backend/ffpy.py
@@ -1,13 +1,16 @@
 import cv2
-# from ffpyplayer import player
+from ffpyplayer.player import MediaPlayer
 
 video_path  = "video.mp4"
 
 video = cv2.VideoCapture(video_path)
-player = ffpyplayer.MediaPlayer(video_path)
+
+player = MediaPlayer(video_path)
 while True:
     grabbed, frame = video.read()
     audio_frame, val = player.get_frame()
+    print(audio_frame)
+    print(val)
     if not grabbed:
         break
     if cv2.waitKey(28) & 0xFF == ord("q"):
@@ -15,3 +18,6 @@ while True:
     cv2.imshow("video", frame)
     if val != 'eof' and audio_frame is not None:
         img, t = audio_frame
+
+    # print(img)
+    # print(t)
